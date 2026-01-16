6d3216d37c920de6505fa0986f36882848c48470
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-01-16 23:51:41 +0100

    completed padding_rows

--- src/parse/parse_cub.c
+++ src/parse/parse_cub.c
@@ -6,11 +6,49 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 18:47:59 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2026/01/16 22:50:21 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2026/01/16 23:51:03 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	validate_identifiers(t_cub *cub)
+{
+	if (!cub)
+		free_exit(EXIT_FAILURE, cub, FAIL_ALLOC, NULL);
+	if (!cub->map->no || !cub->map->so
+		|| !cub->map->we || !cub->map->ea)
+		free_exit(EXIT_FAILURE, cub, "No textures", NULL);
+	if (cub->map->floor_color == -1 || cub->map->ceiling_color == -1)
+		free_exit(EXIT_FAILURE, cub, "No colors", NULL);
+}
+
+static void	padding_rows(t_cub *cub)
+{
+	int		y;
+	int		len;
+	char	*row;
+
+	y = 0;
+	while (y < cub->map.height)
+	{
+		if (!cub->map.matrix[y])
+			free_exit(EXIT_FAILURE, cub, FAIL_ALLOC, NULL);
+		len = ft_strlen(cub->map.matrix[y]);
+		if (len < cub->map.width)
+		{
+			row = ft_calloc(sizeof(char), cub->map.width + 1);
+			if (!row)
+				free_exit(EXIT_FAILURE, cub, "Malloc error in padding", NULL);
+			row = ft_memset(row, ' ', cub->map.width);
+			row = ft_memcpy(row, cub->map.matrix[y], len);
+			row[cub->map.width] = '\0';
+			free(cub->map.matrix[y]);
+			cub->map.matrix[y] = row;
+		}
+		y++;
+	}
+}
 
 void	parse_cub(t_cub *cub, char *file)
 {
@@ -19,5 +57,6 @@ void	parse_cub(t_cub *cub, char *file)
 	get_lines(cub, file);
 	find_map_start(cub);
 	parse_map(cub);
-	//
+	validate_identifiers(cub);
+	padding_rows(cub);
 }
